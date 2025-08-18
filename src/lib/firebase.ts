29621b8b3593bc1022faeafcf83c47057dcbece4
Ramesh Dodamani <rameshdodamani223@gmail.com>
2025-08-18 09:14:12 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,12 +8,12 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyDtM4lGG4IAnhPRmx10fhI7NjMUy_oq25k",
+  authDomain: "creator-canvas-w47i3.firebaseapp.com",
+  projectId: "creator-canvas-w47i3",
+  storageBucket: "creator-canvas-w47i3.appspot.com",
+  messagingSenderId: "705093519264",
+  appId: "1:705093519264:web:dc72ed537b6a83454746c5"
 };
 
 // Initialize Firebase
