432f8eca4a2fe7086b6863429972f943982e52db
Ramesh Dodamani <rameshdodamani223@gmail.com>
2025-08-18 09:52:18 +0000

    error failed to create post

--- src/app/(app)/create/actions.ts
+++ src/app/(app)/create/actions.ts
@@ -1,8 +1,22 @@
 'use server';
 
-import { storage } from '@/lib/firebase';
-import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { randomUUID } from 'crypto';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const storage = getStorage(app);
 
 export async function uploadPhoto(formData: FormData) {
   const file = formData.get('imageFile') as File;
