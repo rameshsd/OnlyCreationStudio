29c4996831379526d3011a8f22820d200deb424b
Ramesh Dodamani <rameshdodamani223@gmail.com>
2025-08-18 11:26:13 +0000

    Error: Failed to parse private key: Error: Invalid PEM formatted message

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,15 +1,9 @@
 
 import admin from 'firebase-admin';
 import { getApps } from 'firebase-admin/app';
+import { serviceAccount } from './service-account';
 
 if (getApps().length === 0) {
-  const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
-  if (!serviceAccountString) {
-    throw new Error('The FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not set.');
-  }
-  
-  const serviceAccount = JSON.parse(serviceAccountString);
-
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
