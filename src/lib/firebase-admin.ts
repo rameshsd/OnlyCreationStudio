d07311ef242a4724cc1186640c720f2e0763ed01
Ramesh Dodamani <rameshdodamani223@gmail.com>
2025-08-18 09:37:45 +0000

    SyntaxError: Unexpected end of JSON input     at JSON.parse (<anonymous>

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,5 +1,9 @@
 import * as admin from 'firebase-admin';
 
+if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+    throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not set. Please check your .env file.');
+}
+
 const serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string
 );
