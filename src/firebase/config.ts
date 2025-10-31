8954833c8bb8a4ae1930145eb5d420bc883201c4
Ramesh Dodamani <rameshdodamani223@gmail.com>
2025-10-31 16:38:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "creator-canvas-w47i3",
+  "appId": "1:705093519264:web:dc72ed537b6a83454746c5",
+  "apiKey": "AIzaSyDtM4lGG4IAnhPRmx10fhI7NjMUy_oq25k",
+  "authDomain": "creator-canvas-w47i3.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "705093519264"
+};
