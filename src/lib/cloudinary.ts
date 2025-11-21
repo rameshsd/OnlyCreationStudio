b5463d1b6805fc7b225c5fb33130cc33af5ed809
Ramesh Dodamani <rameshdodamani223@gmail.com>
2025-11-21 13:22:16 +0000

    Error: Server error: Upload failed: Invalid Signature cdb4f676e28767b797

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,6 +1,8 @@
 
 import { v2 as cloudinary } from 'cloudinary';
 
+// Configure Cloudinary with your credentials
+// This ensures that the environment variable is read and applied before the export.
 cloudinary.config({ 
   cloud_name: 'dkmgby1tc', 
   api_key: '866268445612429', 
