d74a6feff5046df1671510c1175af3f8bc159071
Ramesh Dodamani <rameshdodamani223@gmail.com>
2025-11-21 13:14:31 +0000

    Root Root
    Nov 21, 2025
    866268445612429
    
    PD3Uy5k496xi2dx1VTtowvf0kbI
    
    A

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,6 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({ 
+  cloud_name: 'dkmgby1tc',
+  api_key: '866268445612429',
+  api_secret: 'PD3Uy5k496xi2dx1VTtowvf0kbI',
   secure: true 
 });
 
