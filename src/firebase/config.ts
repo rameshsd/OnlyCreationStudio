3e3d795ede05659f2dc8e0c42d2852e2b4774bc7
Ramesh Dodamani <rameshdodamani223@gmail.com>
2025-11-24 05:05:13 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyDtM4lGG4IAnhPRmx10fhI7NjMUy_oq25k",
   authDomain: "creator-canvas-w47i3.firebaseapp.com",
   projectId: "creator-canvas-w47i3",
-  storageBucket: "creator-canvas-w47i3.appspot.com",
+  storageBucket: "creator-canvas-w47i3.firebasestorage.app",
   messagingSenderId: "705093519264",
   appId: "1:705093519264:web:dc72ed537b6a83454746c5"
 };
